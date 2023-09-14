1872a654fa6504f3768b1c30e73cf9acb6c951ee
Gaunab <paul@42degreesoffreedom.com>
2023-09-14 11:31:48 +0200

    FIX:UnicodeDecodeError while converting rda files

--- spec2nii/Siemens/rda.py
+++ spec2nii/Siemens/rda.py
@@ -1,4 +1,4 @@
-""" spec2nii module containing functions specific to Siemens rda format
+"""spec2nii module containing functions specific to Siemens rda format
 Author: William Clarke <william.clarke@ndcn.ox.ac.uk>
 Copyright (C) 2022 University of Oxford
 """
@@ -11,7 +11,9 @@ import numpy as np
 from nifti_mrs.create_nmrs import gen_nifti_mrs_hdr_ext
 from nifti_mrs.hdr_ext import Hdr_Ext
 
-from spec2nii.dcm2niiOrientation.orientationFuncs import dcm_to_nifti_orientation
+from spec2nii.dcm2niiOrientation.orientationFuncs import (
+    dcm_to_nifti_orientation,
+)
 from spec2nii import __version__ as spec2nii_ver
 
 
@@ -44,57 +46,96 @@ def convert_rda(rda_path, fname_out, verbose):
 
     with open(rda_path, 'rb') as fp:
         for line in fp:
-            if hdr_st.search(line.decode()):
-                pass
-                # print('header found')
-            elif hdr_end.search(line.decode()):
-                # print('header end')
-                break
-            else:
-                match = hdr_val.search(line.decode())
-                if len(match.groups()) < 2:
-                    hdr[match[1]] = None
+            try:
+                if hdr_st.search(line.decode()):
+                    pass
+                    # print('header found')
+                elif hdr_end.search(line.decode()):
+                    # print('header end')
+                    break
                 else:
-                    hdr[match[1]] = match[2]
+                    match = hdr_val.search(line.decode('latin-1'))
+                    if len(match.groups()) < 2:
+                        hdr[match[1]] = None
+                    else:
+                        hdr[match[1]] = match[2]
+            except UnicodeDecodeError:
+                if hdr_st.search(line.decode('latin-1')):
+                    pass
+                    # print('header found')
+                elif hdr_end.search(line.decode('latin-1')):
+                    # print('header end')
+                    break
+                else:
+                    match = hdr_val.search(line.decode('latin-1'))
+                    if len(match.groups()) < 2:
+                        hdr[match[1]] = None
+                    else:
+                        hdr[match[1]] = match[2]
+
         if verbose:
             print(hdr)
 
         data = np.fromfile(fp)
 
-    if (int(hdr['CSIMatrixSize[0]'])
-            * int(hdr['CSIMatrixSize[1]'])
-            * int(hdr['CSIMatrixSize[2]']))\
-            > 1:
-        raise MRSINotHandledError('MRSI is currently not handled in the RDA format. Test data needed.')
+    if (
+        int(hdr['CSIMatrixSize[0]'])
+        * int(hdr['CSIMatrixSize[1]'])
+        * int(hdr['CSIMatrixSize[2]'])
+    ) > 1:
+        raise MRSINotHandledError(
+            'MRSI is currently not handled in the RDA format. Test data needed.'
+        )
 
     data_cmplx = data[0::2] + 1j * data[1::2]
     data_cmplx = data_cmplx.reshape((1, 1, 1) + data_cmplx.shape)
-    dwelltime = _locale_float(hdr['DwellTime']) / 1E6
+    dwelltime = _locale_float(hdr['DwellTime']) / 1e6
 
     warnings.warn(
         'The orientation calculations for rda data is mostly untested.'
-        ' Please contribute test data if you can!')
+        ' Please contribute test data if you can!'
+    )
 
-    imagePositionPatient = np.asarray([
-        _locale_float(hdr['PositionVector[0]']),
-        _locale_float(hdr['PositionVector[1]']),
-        _locale_float(hdr['PositionVector[2]'])])
+    imagePositionPatient = np.asarray(
+        [
+            _locale_float(hdr['PositionVector[0]']),
+            _locale_float(hdr['PositionVector[1]']),
+            _locale_float(hdr['PositionVector[2]']),
+        ]
+    )
 
-    imageOrientationPatient = np.asarray([
-        [_locale_float(hdr['RowVector[0]']), _locale_float(hdr['ColumnVector[0]'])],
-        [_locale_float(hdr['RowVector[1]']), _locale_float(hdr['ColumnVector[1]'])],
-        [_locale_float(hdr['RowVector[2]']), _locale_float(hdr['ColumnVector[2]'])]]).T
+    imageOrientationPatient = np.asarray(
+        [
+            [
+                _locale_float(hdr['RowVector[0]']),
+                _locale_float(hdr['ColumnVector[0]']),
+            ],
+            [
+                _locale_float(hdr['RowVector[1]']),
+                _locale_float(hdr['ColumnVector[1]']),
+            ],
+            [
+                _locale_float(hdr['RowVector[2]']),
+                _locale_float(hdr['ColumnVector[2]']),
+            ],
+        ]
+    ).T
 
-    xyzMM = np.asarray([
-        _locale_float(hdr['PixelSpacingRow']),
-        _locale_float(hdr['PixelSpacingCol']),
-        _locale_float(hdr['SliceThickness'])])
+    xyzMM = np.asarray(
+        [
+            _locale_float(hdr['PixelSpacingRow']),
+            _locale_float(hdr['PixelSpacingCol']),
+            _locale_float(hdr['SliceThickness']),
+        ]
+    )
 
-    currNiftiOrientation = dcm_to_nifti_orientation(imageOrientationPatient,
-                                                    imagePositionPatient,
-                                                    xyzMM,
-                                                    (1, 1, 1),
-                                                    verbose=verbose)
+    currNiftiOrientation = dcm_to_nifti_orientation(
+        imageOrientationPatient,
+        imagePositionPatient,
+        xyzMM,
+        (1, 1, 1),
+        verbose=verbose,
+    )
 
     meta = extractRdaMetadata(hdr)
     meta.set_standard_def('OriginalFile', [rda_path.name])
@@ -104,7 +145,13 @@ def convert_rda(rda_path, fname_out, verbose):
     else:
         name = rda_path.stem
 
-    return [gen_nifti_mrs_hdr_ext(data_cmplx, dwelltime, meta, currNiftiOrientation.Q44, no_conj=True), ], [name, ]
+    return [
+        gen_nifti_mrs_hdr_ext(
+            data_cmplx, dwelltime, meta, currNiftiOrientation.Q44, no_conj=True
+        ),
+    ], [
+        name,
+    ]
 
 
 def extractRdaMetadata(hdr):
@@ -117,14 +164,11 @@ def extractRdaMetadata(hdr):
     """
 
     # Extract required metadata and create hdr_ext object
-    obj = Hdr_Ext(
-        _locale_float(hdr['MRFrequency']),
-        hdr['Nucleus'])
+    obj = Hdr_Ext(_locale_float(hdr['MRFrequency']), hdr['Nucleus'])
 
     # Standard defined metadata
     def set_standard_def(nifti_mrs_key, location, key, cast=None):
-        if key in location\
-                and location[key] is not None:
+        if key in location and location[key] is not None:
             if cast is not None:
                 obj.set_standard_def(nifti_mrs_key, cast(location[key]))
             else:
@@ -132,15 +176,17 @@ def extractRdaMetadata(hdr):
 
     # # 5.1 MRS specific Tags
     # 'EchoTime'
-    obj.set_standard_def('EchoTime', _locale_float(hdr['TE']) * 1E-3)
+    obj.set_standard_def('EchoTime', _locale_float(hdr['TE']) * 1e-3)
     # 'RepetitionTime'
-    obj.set_standard_def('RepetitionTime', _locale_float(hdr['TR']) * 1E-3)
+    obj.set_standard_def('RepetitionTime', _locale_float(hdr['TR']) * 1e-3)
     # 'InversionTime'
-    obj.set_standard_def('InversionTime', _locale_float(hdr['TI']) * 1E-3)
+    obj.set_standard_def('InversionTime', _locale_float(hdr['TI']) * 1e-3)
     # 'MixingTime'
-    obj.set_standard_def('MixingTime', _locale_float(hdr['TM']) * 1E-3)
+    obj.set_standard_def('MixingTime', _locale_float(hdr['TM']) * 1e-3)
     # 'ExcitationFlipAngle'
-    obj.set_standard_def('ExcitationFlipAngle', _locale_float(hdr['FlipAngle']))
+    obj.set_standard_def(
+        'ExcitationFlipAngle', _locale_float(hdr['FlipAngle'])
+    )
     # 'TxOffset'
     # 'VOI'
     # 'WaterSuppressed'
@@ -185,7 +231,9 @@ def extractRdaMetadata(hdr):
     # # 5.5 Provenance and conversion metadata
     obj.set_standard_def('ConversionMethod', f'spec2nii v{spec2nii_ver}')
     # 'ConversionTime'
-    conversion_time = datetime.now().isoformat(sep='T', timespec='milliseconds')
+    conversion_time = datetime.now().isoformat(
+        sep='T', timespec='milliseconds'
+    )
     obj.set_standard_def('ConversionTime', conversion_time)
 
     # 'OriginalFile'
