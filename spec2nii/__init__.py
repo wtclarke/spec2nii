a223283c380e6c968b6ed8cc257d3f96c99c24c9
Marcel Zwiers <m.zwiers@donders.ru.nl>
2023-11-12 16:39:24 +0100

    Tiny bugfix

--- spec2nii/__init__.py
+++ spec2nii/__init__.py
@@ -5,4 +5,4 @@ __version__ = _version.get_versions()['version']
 
 # Register the duecredit citation for spec2nii
 due.cite(Doi('10.1002/mrm.29418'), description='Multi-format in vivo MR spectroscopy conversion to NIFTI',
-         path='spec2nii', version=__version__, tag='reference-implementation', cite_module=True)
+         path='spec2nii', version=__version__, tags=['reference-implementation'], cite_module=True)
