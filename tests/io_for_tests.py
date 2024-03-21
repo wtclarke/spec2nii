e993d46c0a595e7d54f6a2bca7590d7602f290ee
Mark Mikkelsen <mark.mikkelsen@gmail.com>
2024-03-21 12:08:11 -0400

    Enhance support for GE data (#132)
    * Update .gitignore
    
    * Some cosmetic edits based on PEP 8 and grammar
    
    * Add cv24 variable that is important for advanced options in GE data
    
    * Spelling corrections
    
    * Update ge_read_pfile.py
    
    Add an if-then statement for sLASER datasets where the receiver phase was set
    
    * Refined handling of edited GE data
    
    * Update `ge_hdr_fields.py`
    
    Added missing header fields for revisions 14, 15, and 16
    
    * Update ge_read_pfile.py
    
    Cosmetic edits to code
    
    * Better handling of GE psd strings
    
    - Cleaned up code a bit in `ge_read_pfile.py`
    - Added support for a few other psd's
    
    * Add support of GE header revision 14.3
    
    * Bug fix in `ge_read_pfile.py`
    
    The last block of lines was previously within the `else` statement just above, which prevented those lines from running when `nechoes == 1`
    
    * Bug fix in `ge_read_pfile.py`
    
    Incorrect header fields were being used to determine `dataframes` and `refframes`
    
    * Update ge_read_pfile.py
    
    - Updated the `mult` and `multw` factors to match the current ones used in Gannet [NOTE: These have still not been completely finalized and may need to be adjusted in the future]
    - Some cosmetic edits
    
    * Cosmetic edits
    
    * Add tests to MMs GE edits
    
    * Tweak test
    
    * Fix for the editing disabled jpress case.
    
    * Add MEGA editing pulse information to the header.
    
    ---------
    
    Co-authored-by: wtclarke <william.clarke@ndcn.ox.ac.uk>

--- tests/io_for_tests.py
+++ tests/io_for_tests.py
@@ -1,4 +1,5 @@
 import nibabel as nib
+import json
 
 
 def read_nifti_mrs(file_path):
@@ -7,3 +8,14 @@ def read_nifti_mrs(file_path):
     img = nib.load(str(file_path))
 
     return img
+
+
+def read_nifti_mrs_with_hdr(file_path):
+    '''Read NIFTI MRS files using nibabel.
+    Return img and header extracted.'''
+
+    img = nib.load(str(file_path))
+    hdr_ext_codes = img.header.extensions.get_codes()
+    hdr_ext = json.loads(img.header.extensions[hdr_ext_codes.index(44)].get_content())
+
+    return img, hdr_ext
--- tests/test_ge_pfile.py
+++ tests/test_ge_pfile.py
@@ -11,12 +11,26 @@ import json
 import numpy as np
 
 from .io_for_tests import read_nifti_mrs
+from .io_for_tests import read_nifti_mrs_with_hdr
+
 
 # Data paths
 ge_path = Path(__file__).parent / 'spec2nii_test_data' / 'ge'
 svs_path = ge_path / 'pFiles' / 'svMRS' / 'P03072.7'
 mrsi_path = ge_path / 'pFiles' / 'MRSI' / 'P18432.7'
 mpress_path = ge_path / 'pFiles' / 'big_gaba' / 'S01_GABA_68.7'
+
+# Test set from Mark Mikkelsen
+mm_herc = [
+    ge_path / 'pFiles' / 'HERCULES' / 'sub-01_ses-01_acq-herculespress_svs_noID.7',
+    ge_path / 'pFiles' / 'HERCULES' / 'sub-01_ses-01_acq-herculesslaser_svs_noID.7']
+mm_hermes = [
+    ge_path / 'pFiles' / 'HERMES' / 'sub-01_ses-01_acq-hermespress_svs_noID.7',
+    ge_path / 'pFiles' / 'HERMES' / 'sub-01_ses-01_acq-hermesslaser_svs_noID.7']
+mm_mega = ge_path / 'pFiles' / 'MEGA' / 'Big_GABA'
+mm_press = ge_path / 'pFiles' / 'PRESS' / 'Big_GABA'
+mm_press_noid = ge_path / 'pFiles' / 'PRESS' / 'sub-01_ses-01_acq-press_svs_noID.7'
+mm_slaser = ge_path / 'pFiles' / 'sLASER' / 'sub-01_ses-01_acq-slaser_svs_noID.7'
 
 
 def test_svs(tmp_path):
@@ -104,3 +118,207 @@ def test_mpress(tmp_path):
 
     assert np.isclose(hdr_ext['EchoTime'], 0.068)
     assert np.isclose(hdr_ext['RepetitionTime'], 2.0)
+
+
+def test_mm_hercules(tmp_path):
+    for path in mm_herc:
+        subprocess.run([
+            'spec2nii', 'ge',
+            '-f', path.stem,
+            '-o', tmp_path,
+            '-j',
+            path])
+
+        img, hdr_ext = read_nifti_mrs_with_hdr(
+            tmp_path / path.with_suffix('.nii.gz').name)
+
+        assert img.shape == (1, 1, 1, 4096, 32, 56, 4)
+        assert np.iscomplexobj(img.dataobj)
+        assert 1 / img.header['pixdim'][4] == 5000.0
+        assert hdr_ext['dim_5'] == 'DIM_COIL'
+        assert hdr_ext['dim_6'] == 'DIM_DYN'
+        assert hdr_ext['dim_7'] == 'DIM_EDIT'
+        assert np.isclose(127.771, hdr_ext['SpectrometerFrequency'][0], atol=1E-3)
+        assert hdr_ext['ResonantNucleus'][0] == '1H'
+        assert hdr_ext['OriginalFile'][0] == path.name
+        assert hdr_ext['WaterSuppressed']
+
+        # Test that the MEGA editing header info is not added
+        assert 'dim_7_header' not in hdr_ext
+        assert 'EditPulse' not in hdr_ext
+
+        img_ref, hdr_ext_ref = read_nifti_mrs_with_hdr(
+            tmp_path / f'{path.stem}_ref.nii.gz')
+        assert img_ref.shape[:5] == (1, 1, 1, 4096, 32)
+        assert img_ref.shape[6] == 4
+        assert not hdr_ext_ref['WaterSuppressed']
+        assert hdr_ext_ref['dim_5'] == 'DIM_COIL'
+        assert hdr_ext_ref['dim_6'] == 'DIM_DYN'
+        assert hdr_ext_ref['dim_7'] == 'DIM_EDIT'
+
+
+def test_mm_hermes(tmp_path):
+    for path in mm_hermes:
+        subprocess.run([
+            'spec2nii', 'ge',
+            '-f', path.stem,
+            '-o', tmp_path,
+            '-j',
+            path])
+
+        img, hdr_ext = read_nifti_mrs_with_hdr(
+            tmp_path / path.with_suffix('.nii.gz').name)
+
+        assert img.shape == (1, 1, 1, 4096, 32, 56, 4)
+        assert np.iscomplexobj(img.dataobj)
+        assert 1 / img.header['pixdim'][4] == 5000.0
+        assert hdr_ext['dim_5'] == 'DIM_COIL'
+        assert hdr_ext['dim_6'] == 'DIM_DYN'
+        assert hdr_ext['dim_7'] == 'DIM_EDIT'
+        assert np.isclose(127.771, hdr_ext['SpectrometerFrequency'][0], atol=1E-3)
+        assert hdr_ext['ResonantNucleus'][0] == '1H'
+        assert hdr_ext['OriginalFile'][0] == path.name
+        assert hdr_ext['WaterSuppressed']
+
+        assert 'dim_7_header' not in hdr_ext
+        assert 'EditPulse' not in hdr_ext
+
+        img_ref, hdr_ext_ref = read_nifti_mrs_with_hdr(
+            tmp_path / f'{path.stem}_ref.nii.gz')
+        assert img_ref.shape[:5] == (1, 1, 1, 4096, 32)
+        assert img_ref.shape[6] == 4
+        assert not hdr_ext_ref['WaterSuppressed']
+        assert hdr_ext_ref['dim_5'] == 'DIM_COIL'
+        assert hdr_ext_ref['dim_6'] == 'DIM_DYN'
+        assert hdr_ext_ref['dim_7'] == 'DIM_EDIT'
+
+
+def test_mm_mega(tmp_path):
+    for path in mm_mega.rglob('*.7'):
+        subprocess.run([
+            'spec2nii', 'ge',
+            '-f', path.stem,
+            '-o', tmp_path,
+            '-j',
+            path])
+
+        img, hdr_ext = read_nifti_mrs_with_hdr(
+            tmp_path / path.with_suffix('.nii.gz').name)
+
+        assert img.shape[:3] == (1, 1, 1)
+        assert img.shape[3] in (2048, 4096)
+        assert img.shape[6] == 2
+        assert np.iscomplexobj(img.dataobj)
+        assert 1 / img.header['pixdim'][4] in (2000.0, 5000.0)
+        assert hdr_ext['dim_5'] == 'DIM_COIL'
+        assert hdr_ext['dim_6'] == 'DIM_DYN'
+        assert hdr_ext['dim_7'] == 'DIM_EDIT'
+        assert np.isclose(127.7, hdr_ext['SpectrometerFrequency'][0], atol=1E-1)
+        assert hdr_ext['ResonantNucleus'][0] == '1H'
+        assert hdr_ext['OriginalFile'][0] == path.name
+        assert hdr_ext['WaterSuppressed']
+
+        assert 'dim_7_header' in hdr_ext
+        assert 'EditPulse' in hdr_ext
+        assert 'dim_7_info' in hdr_ext
+
+        img_ref, hdr_ext_ref = read_nifti_mrs_with_hdr(
+            tmp_path / f'{path.stem}_ref.nii.gz')
+        assert img_ref.shape[:3] == (1, 1, 1)
+        assert img_ref.shape[6] == 2
+        # Check reference has same number of points and coils as main without checking number of coils
+        assert img_ref.shape[3] == img.shape[3]
+        assert img_ref.shape[4] == img.shape[4]
+        assert not hdr_ext_ref['WaterSuppressed']
+        assert hdr_ext_ref['dim_5'] == 'DIM_COIL'
+        assert hdr_ext_ref['dim_6'] == 'DIM_DYN'
+        assert hdr_ext_ref['dim_7'] == 'DIM_EDIT'
+
+
+def test_mm_press(tmp_path):
+    for path in mm_press.rglob('*.7'):
+        subprocess.run([
+            'spec2nii', 'ge',
+            '-f', path.stem,
+            '-o', tmp_path,
+            '-j',
+            path])
+
+        img, hdr_ext = read_nifti_mrs_with_hdr(
+            tmp_path / path.with_suffix('.nii.gz').name)
+
+        assert img.shape[:3] == (1, 1, 1)
+        assert img.shape[3] in (2048, 4096)
+        assert np.iscomplexobj(img.dataobj)
+        assert 1 / img.header['pixdim'][4] in (2000.0, 5000.0)
+        assert hdr_ext['dim_5'] == 'DIM_COIL'
+        assert hdr_ext['dim_6'] == 'DIM_DYN'
+
+        assert np.isclose(127.7, hdr_ext['SpectrometerFrequency'][0], atol=1E-1)
+        assert hdr_ext['ResonantNucleus'][0] == '1H'
+        assert hdr_ext['OriginalFile'][0] == path.name
+        assert hdr_ext['WaterSuppressed']
+
+        img_ref, hdr_ext_ref = read_nifti_mrs_with_hdr(
+            tmp_path / f'{path.stem}_ref.nii.gz')
+        assert img_ref.shape[:3] == (1, 1, 1)
+        # Check reference has same number of points and coils as main without checking actual number
+        assert img_ref.shape[3] == img.shape[3]
+        assert img_ref.shape[4] == img.shape[4]
+        assert not hdr_ext_ref['WaterSuppressed']
+        assert hdr_ext_ref['dim_5'] == 'DIM_COIL'
+        assert hdr_ext_ref['dim_6'] == 'DIM_DYN'
+
+
+def test_mm_press_noid(tmp_path):
+    subprocess.run([
+        'spec2nii', 'ge',
+        '-f', mm_press_noid.stem,
+        '-o', tmp_path,
+        '-j',
+        mm_press_noid])
+
+    img, hdr_ext = read_nifti_mrs_with_hdr(
+        tmp_path / mm_press_noid.with_suffix('.nii.gz').name)
+
+    assert img.shape == (1, 1, 1, 4096, 32, 64)
+    assert np.iscomplexobj(img.dataobj)
+    assert 1 / img.header['pixdim'][4] == 5000.0
+    assert hdr_ext['dim_5'] == 'DIM_COIL'
+    assert hdr_ext['dim_6'] == 'DIM_DYN'
+    assert np.isclose(127.771, hdr_ext['SpectrometerFrequency'][0], atol=1E-3)
+    assert hdr_ext['ResonantNucleus'][0] == '1H'
+    assert hdr_ext['OriginalFile'][0] == mm_press_noid.name
+
+    img_ref, hdr_ext_ref = read_nifti_mrs_with_hdr(
+        tmp_path / f'{mm_press_noid.stem}_ref.nii.gz')
+    assert img_ref.shape[:5] == (1, 1, 1, 4096, 32)
+    assert not hdr_ext_ref['WaterSuppressed']
+    assert hdr_ext_ref['dim_5'] == 'DIM_COIL'
+
+
+def test_mm_slaser(tmp_path):
+    subprocess.run([
+        'spec2nii', 'ge',
+        '-f', mm_slaser.stem,
+        '-o', tmp_path,
+        '-j',
+        mm_slaser])
+
+    img, hdr_ext = read_nifti_mrs_with_hdr(
+        tmp_path / mm_slaser.with_suffix('.nii.gz').name)
+
+    assert img.shape == (1, 1, 1, 4096, 32, 64)
+    assert np.iscomplexobj(img.dataobj)
+    assert 1 / img.header['pixdim'][4] == 5000.0
+    assert hdr_ext['dim_5'] == 'DIM_COIL'
+    assert hdr_ext['dim_6'] == 'DIM_DYN'
+    assert np.isclose(127.771, hdr_ext['SpectrometerFrequency'][0], atol=1E-3)
+    assert hdr_ext['ResonantNucleus'][0] == '1H'
+    assert hdr_ext['OriginalFile'][0] == mm_slaser.name
+
+    img_ref, hdr_ext_ref = read_nifti_mrs_with_hdr(
+        tmp_path / f'{mm_slaser.stem}_ref.nii.gz')
+    assert img_ref.shape[:5] == (1, 1, 1, 4096, 32)
+    assert not hdr_ext_ref['WaterSuppressed']
+    assert hdr_ext_ref['dim_5'] == 'DIM_COIL'
